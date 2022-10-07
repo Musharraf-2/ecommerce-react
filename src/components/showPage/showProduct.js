3cd1c6f2405ac31286c652e158bd309f7fb7e880
Musharraf-2 <musharraf.tahir@devsinc.com>
2022-10-07 20:43:24 +0500

    Handled invalid product id route * showProducts.js: added check to show invalid product id meesage

--- src/components/showPage/showProduct.js
+++ src/components/showPage/showProduct.js
@@ -20,26 +20,30 @@ function ShowProduct() {
     <div className="container mt-5">
       {!isLoaded ? <h4 className='text-center'>Loading...</h4> :
         <div>
-          <h1 className="text-center">{product.title}</h1>
-          <strong>Description: </strong>{product.description}<br />
-          <strong>Price: </strong>{product.price} $<br />
-          <strong>Quantity: </strong>{product.quantity}<br />
-          <strong>Serial Number: </strong>{product.serial_number}<br />
-          <div className="d-flex flex-row justify-content-center">
-            {product.quantity > 0 ? <button className=" btn btn-primary p-2 rounded mx-2 my-4">Add to Cart</button>
-              : <p className="text-danger m-2 my-4">Currently out of stock.</p>}
-          </div>
-          <div className="row justify-content-center">
-            <div className="col-4">
-              {product.images_path && <Carousel images_path={product.images_path} />}
+          {product ?
+            <div>
+              <h1 className="text-center">{product.title}</h1>
+              <strong>Description: </strong>{product.description}<br />
+              <strong>Price: </strong>{product.price} $<br />
+              <strong>Quantity: </strong>{product.quantity}<br />
+              <strong>Serial Number: </strong>{product.serial_number}<br />
+              <div className="d-flex flex-row justify-content-center">
+                {product.quantity > 0 ? <button className=" btn btn-primary p-2 rounded mx-2 my-4">Add to Cart</button>
+                  : <p className="text-danger m-2 my-4">Currently out of stock.</p>}
+              </div>
+              <div className="row justify-content-center">
+                <div className="col-4">
+                  <Carousel images_path={product.images_path} />
+                </div>
+              </div>
+              <h1 className="text-center my-5">Comments</h1>
+              <div className="comments container border p-2 mb-4">
+                {product.comments.map((comment) => {
+                  return <Comment comment={comment} key={comment.id} />
+                })}
+              </div>
             </div>
-          </div>
-          <h1 className="text-center my-5">Comments</h1>
-          <div className="comments container border p-2 mb-4">
-            {product.comments && product.comments.map((comment) => {
-              return <Comment comment={comment} key={comment.id} />
-            })}
-          </div>
+            : <h2 className='text-center'>The product you are asking does not exist.</h2>}
         </div>
       }
     </div>
